b53f2ec1f17985439b692d3a1ce0db4f3c1c1c9a
Vincent Lin <soulace2425@outlook.com>
2022-09-14 16:58:41 -0700

    Add -Register option that auto adds to registry

--- format-spaces/format.ps1
+++ format-spaces/format.ps1
@@ -1,9 +1,54 @@
 <#
 .Synopsis
-   Formats delimiters of file names.
+    Formats delimiters of file names.
 .DESCRIPTION
-   To be used from the context menu. Upon running this script, standardizes the delimitation of words in the names of the files in the current directory.
+    To be used from the context menu. To add this script to the context menu, run this script with the -Register switch. When run normally afterwards, this script standardizes the delimitation of words in the names of the files in the current directory.
+.PARAMETER Register
+    If this switch is included, the main program is not run. Instead, the script registers itself in the registry to make it available from the context menu
+.NOTES
+    Author: Vincent Lin
+    Date:   14 September 2022
 #>
+
+param (
+    [Parameter()]
+    [switch] $Register
+)
+
+<# Add script to registry if run with -Register switch #>
+
+if ($Register) {
+    $HKCU_PATH = "HKCU:\Software\Classes\Directory\Background\shell\"
+    
+    # Ask to overwrite the existing key if already exists
+    $appPath = Join-Path $HKCU_PATH "FormatSpaces"
+    if (Test-Path $appPath) {
+        Write-Host "format-spaces is already registered in the registry. Would you like to overwrite it? (y/N) " -NoNewline -ForegroundColor Yellow
+        $confirmation = Read-Host
+        if ($confirmation -ne "y") {
+            exit 0
+        }
+        Remove-Item $appPath -Recurse
+    }
+
+    # Create the container for the app
+    Write-Host "Adding format-spaces to the registry..." -ForegroundColor Yellow
+    New-Item -Path $appPath
+    New-ItemProperty -Path $appPath -Name "(Default)" -Value "Format file names"
+
+    # Create the command subkey pointed to this script
+    $commandPath = Join-Path $appPath "command"
+    New-Item -Path $commandPath
+    $__file__ = $MyInvocation.MyCommand.Path
+    $command = "powershell.exe -File $__file__ -WindowStyle Hidden"
+    New-ItemProperty -Path $commandPath -Name "(Default)" -Value $command
+
+    Write-Host "Key points to the current location of this script: $__file__. If you move this script, be sure to run it with the -Register flag again!" -ForegroundColor Yellow
+    Write-Host "Added format-spaces to the registry." -ForegroundColor Green
+    exit 0
+}
+
+<# Otherwise, run the main process #>
 
 # Delimiter used to separate words in final name
 $WORD_DELIMITER = "-"
