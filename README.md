e44fee9a6370b24de6ccbe74fabc55894236e62c
Vincent Lin <soulace2425@outlook.com>
2022-09-18 22:37:38 -0700

    Add root README and automation script

---
+++ README.md
@@ -0,0 +1,18 @@
+# scripts
+
+This repository includes a collection of simple PowerShell scripts that
+I've written to automate certain tasks. Most of these also come with
+installation instructions for if you, a lucky passerby, want to make
+your Windows workflow just a bit more convenient too!
+
+Check them out here:
+
+- [Format Spaces](desktop-shortcut)
+- [Downloads Paster](downloads-paster)
+- [Format Spaces](format-spaces)
+
+
+I also linked this repository to my
+[PowerShell profile script backup](PS-profile) since I thought it
+seemed relevant. There you can see the kind of convenience functions
+I've written to improve my own command line workflow!
---
+++ update_readme.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""update_readme.py
+
+Script to run upon push. Updates the root README.md document to include
+links to the subdirectories.
+"""
+
+from pathlib import Path
+
+# Assert that script is run starting at the project root
+assert Path.cwd() == Path(__file__).parent
+
+print("Running update_readme.py...")
+
+# List of names of directories at the root to exclude.
+EXCLUDE = [
+    ".git",
+    "PS-profile",
+    "test",
+]
+
+# Path to the README.md document to update.
+ROOT_README = Path("README.md")
+
+# Markdown template
+markdown = """\
+# scripts
+
+This repository includes a collection of simple PowerShell scripts that
+I've written to automate certain tasks. Most of these also come with
+installation instructions for if you, a lucky passerby, want to make
+your Windows workflow just a bit more convenient too!
+
+Check them out here:
+
+{links}
+
+I also linked this repository to my
+[PowerShell profile script backup](PS-profile) since I thought it
+seemed relevant. There you can see the kind of convenience functions
+I've written to improve my own command line workflow!
+"""
+
+# Populate the links placeholder
+
+buffer = ""
+for file in Path.cwd().iterdir():
+    if not file.is_dir() or file.name in EXCLUDE:
+        continue
+
+    # Extract the program's name from its README
+    readme = file / "README.md"
+    with open(readme, "rt", encoding="utf-8") as fp:
+        header = fp.readline().strip()
+    program_name = header.removeprefix("# ")
+
+    # Markdown bulleted list
+    buffer += f"- [{program_name}]({file.name})\n"
+
+buffer = buffer or "- Nothing yet, sit tight!\n"
+markdown = markdown.format(links=buffer)
+
+# Write to the root README
+with open(ROOT_README, "wt", encoding="utf-8") as fp:
+    fp.write(markdown)
+
+print("Finished running update_readme.py.")
