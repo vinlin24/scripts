d192c1426702e7de6bc3233117bc63ae12da8603
Vincent Lin <soulace2425@outlook.com>
2022-09-18 21:26:14 -0700

    Add docstring and -Register switch

--- desktop-shortcut/shortcut.ps1
+++ desktop-shortcut/shortcut.ps1
@@ -1,8 +1,64 @@
-# Extract selected file from %1 passed from Registry
-$selectedFile = $args[0]
+<#
+.Synopsis
+    Creates a desktop shortcut for a selected file.
+.DESCRIPTION
+    To be used from the context menu. To add this script to the context menu, run this script with the -Register switch. When run normally afterwards, this script creates a shortcut on the user's desktop with the same file name. Notably, it excludes the annoying ' - Shortcut' suffix.
+.PARAMETER File
+    The file to create a desktop shortcut for. Passed automatically when used from the context menu.
+.PARAMETER Register
+    If this switch is included, the main program is not run. Instead, the script registers itself in the registry to make it available from the context menu
+.NOTES
+    Author: Vincent Lin
+    Date:   18 September 2022
+#>
 
-if ($null -eq $selectedFile) {
-    Write-Host "No file selected!" -ForegroundColor Red
+param (
+    # To be passed from "%1" by the Registry
+    [Parameter()]
+    [string] $File,
+    [Parameter()]
+    [switch] $Register
+)
+
+<# Add script to registry if run with -Register switch #>
+
+if ($Register) {
+    # WARNING: Use -LiteralPath for any operations with paths containing *
+    $HKCU_PATH = "HKCU:\Software\Classes\*\shell\"
+    
+    # Ask to overwrite the existing key if already exists
+    $appPath = Join-Path $HKCU_PATH "DesktopShortcut"
+    if (Test-Path -LiteralPath $appPath) {
+        Write-Host "desktop-shortcut is already registered in the registry. Would you like to overwrite it? (y/N) " -NoNewline -ForegroundColor Yellow
+        $confirmation = Read-Host
+        if ($confirmation -ne "y") {
+            exit 0
+        }
+        Remove-Item -LiteralPath $appPath -Recurse
+    }
+
+    # Create the container for the app
+    Write-Host "Adding desktop-shortcut to the registry..." -ForegroundColor Yellow
+    New-Item -Path $appPath
+    New-ItemProperty -LiteralPath $appPath -Name "(Default)" -Value "Add desktop shortcut"
+
+    # Create the command subkey pointed to this script
+    $commandPath = Join-Path $appPath "command"
+    New-Item -Path $commandPath
+    $__file__ = $MyInvocation.MyCommand.Path
+    # "%1" necessary to capture selected file
+    $command = "powershell.exe -WindowStyle Hidden -File $__file__ `"%1`""
+    New-ItemProperty -LiteralPath $commandPath -Name "(Default)" -Value $command
+
+    Write-Host "Key points to the current location of this script: $__file__. If you move this script, be sure to run it with the -Register flag again!" -ForegroundColor Yellow
+    Write-Host "Added desktop-shortcut to the registry." -ForegroundColor Green
+    exit 0
+}
+
+<# Otherwise run normally #>
+
+if ($File -eq "") {
+    Write-Host "No file selected, aborted." -ForegroundColor Red
     exit 1
 }
 
@@ -33,5 +89,5 @@ function Add-DesktopShortcut {
     Write-Host "Created shortcut file." -ForegroundColor Green
 }
 
-Add-DesktopShortcut $selectedFile
+Add-DesktopShortcut $File
 exit 0
